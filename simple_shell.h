d0a378d9b05bfa2024e74fa5542b19af45963fcb
JorreJ <jade.jorre@gmail.com>
2025-01-09 10:52:31 +0100

    part execve in another function

--- simple_shell.h
+++ simple_shell.h
@@ -7,5 +7,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
+
+int _strlen(char *s);
+void exec_command(char *string, char *inter_name);
 
 #endif /* SIMPLESHELL_H */
