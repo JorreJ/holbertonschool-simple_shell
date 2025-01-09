0e96595a64e011c3275a242d19d3b004d4a791f1
Kevindecastro <9830@holbertonstudents.com>
2025-01-09 15:28:56 +0100

    add shell.h

--- shell.h
+++ shell.h
@@ -3,14 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-/* Prototypes des fonctions */
-char *read_prompt(void);
-void execute_command(char *line);
-int _strcmp(const char *s1, const char *s2);
+extern char **environ;
+
+/* Function Prototypes */
+void prompt(void);
+char *read_input(void);
+char **parse_input(char *input);
+int execute(char **args);
+int handle_builtin(char **args);
+char *find_command(char *command);
+void free_args(char **args);
 
 #endif
